dd395c89068c14d5c9304975fa79848a6e0b034b
Yalog Nguyen <nguyenyalog@gmail.com>
2022-03-27 20:27:27 +0700

    first commit YalogNguyen

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>DIGXU</title>
+    <link rel="icon" href="./img/peo.png">
+    <link rel="stylesheet" href="./custom.css">
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script> -->
+</head>
+<body>
+    <div class="banner">
+        <div class="about-sign-in">
+            <a href="#" class="about">About us</a>
+            <a href="#" class="sign-in">Sign in</a>
+        </div>
+        <div class="logo">
+        </div>
+        <div class="content-center">
+            <div class="left">
+                <img src="./img/icon-gift.svg" alt="">
+                <span>Better</span>
+            </div>
+            <div class="center">
+                <img src="./img/icon-clock.svg" alt="">
+                <span>Faster</span>
+            </div>
+            <div class="right">
+                <img src="./img/icon-like.svg" alt="">
+                <span>Together</span>
+            </div>
+        </div>
+        <div class="text">
+            <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry</span>
+        </div>
+        <div class="btn-start-now">
+            <button>Star Now</button>
+        </div>
+    </div>
+</body>
+</html>
